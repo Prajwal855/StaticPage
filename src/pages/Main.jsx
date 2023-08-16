aea7bba5753e864bd6bb4e6199fcd13bf45ac311
Prajwal855 <98931921+Prajwal855@users.noreply.github.com>
2023-08-16 10:19:22 +0530

    Static page

---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import Content from "../components/Content";
+import NavBar from "../components/Navbar";
+
+const Main = ()=>{
+    return(
+        <div className="container">
+            <NavBar />
+            <Content/>
+        </div>
+    )
+}
+
+export default Main;
